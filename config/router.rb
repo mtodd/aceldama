9294a307ff4a9b1f5af10c832dc34dc6ae575f98
Matt Todd <chiology@gmail.com>
2008-09-17 05:37:13 -0400

    Added routes for comments and home.

--- config/router.rb
+++ config/router.rb
@@ -21,9 +21,12 @@
 
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
+  
   # RESTful routes
   # r.resources :posts
-
+  
+  r.resources :comments
+  
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
   # routes, you may want to comment/remove this line to prevent
@@ -31,5 +34,6 @@ Merb::Router.prepare do |r|
   r.default_routes
   
   # Change this for your home page to be available at /
-  # r.match('/').to(:controller => 'whatever', :action =>'index')
+  r.match('/').to(:controller => 'home', :action =>'index')
+  
 end
