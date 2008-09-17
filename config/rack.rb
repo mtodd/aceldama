d31caa812a650cfafd09d7e483800c9ebd552faa
Matt Todd <chiology@gmail.com>
2008-09-17 04:35:25 -0400

    Initial merging of Halcyon and Merb applications.

--- config/rack.rb
+++ config/rack.rb
@@ -1,3 +1,11 @@
+# Make sure that the lib/ folder is in the load path
+$:.unshift(Halcyon.root/'lib')
+
+# Silence Thin's logging
+Thin::Logging.silent = true if defined? Thin
+
+# Configure Halcyon's paths
+
 
 # use PathPrefix Middleware if :path_prefix is set in Merb::Config
 if prefix = ::Merb::Config[:path_prefix]
@@ -8,5 +16,12 @@ end
 # that serves static files
 use Merb::Rack::Static, Merb.dir_for(:public)
 
-# this is our main merb application
-run Merb::Rack::Application.new
+# Set up array of apps for cascading requests.
+apps = []
+
+# Add the apps
+apps << Halcyon::Runner.new
+apps << Merb::Rack::Application.new
+
+# Begin cascading
+run Rack::Cascade.new(apps)
