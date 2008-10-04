816c68e8566a2b6a76f994ced9a33f1994a431fb
Matt Todd <chiology@gmail.com>
2008-10-04 11:18:26 -0400

    Implemented comment serving over JS. (That's like the whole point of this thing, right?)

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -2,8 +2,6 @@ class Comments < Application
 
   provides :js, :json
   
-  # ...and remember, everything returned from an action
-  # goes to the client...
   def index
     @comments = Comment.all
     render
