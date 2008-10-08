615b5e8b73c0f684496b2989996f4e94dac0878a
Matt Todd <chiology@gmail.com>
2008-10-08 01:41:03 -0400

    Added ability to create comments and numerous improvements to the display and error handling abilities.

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,13 +3,53 @@ class Comments < Application
   provides :js, :json
   
   def index
-    @comments = Comment.all
+    # find the page or create it
+    unless page = Page.first(:url => params[:page])
+      page = Page.new(
+        :url => params[:page],
+        :created_at => Time.now,
+        :updated_at => Time.now
+      )
+      page.save
+    end
+    
+    # get comments
+    @comments = page.comments(:order => [:created_at.desc])
     
     case content_type
     when :js
       render
     when :json
+      display
+    end
+  end
+  
+  def create
+    # handle users
+    unless user = User.first(:email => params[:email])
+      user = User.new(
+        :email => params[:email],
+        :created_at => Time.now,
+        :updated_at => Time.now
+      )
+      user.save
+    end
+    
+    # Create comment
+    @comment = Comment.new(
+      :page => Page.first(:url => params[:page]),
+      :body => params[:body],
+      :user => user,
+      :created_at => Time.now,
+      :updated_at => Time.now
+    )
+    @comment.save
+    
+    case content_type
+    when :js
       render
+    when :json
+      display
     end
   end
   
